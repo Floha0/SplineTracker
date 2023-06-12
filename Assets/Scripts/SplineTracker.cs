961aff8e68e5d1f1970df4f353477c395473dd00
Floha <fgulhan07@gmail.com>
2023-06-12 20:09:34 +0300

    Custom Spline Tracker
    https://www.youtube.com/watch?v=gzPjH7-gHLE

--- Assets/Scripts/SplineTracker.cs
+++ Assets/Scripts/SplineTracker.cs
@@ -6,14 +6,27 @@ using UnityEngine.Splines;
 public class SplineTracker : MonoBehaviour
 {
     public SplineContainer spline;
-    private SplineAnimate fishFollow;
+    public float speed;
+    private float splineLength;
+    private float distancePercentage;
+
     void Start()
     {
-        fishFollow = this.GetComponent<SplineAnimate>();
+        splineLength = spline.CalculateLength();
     }
 
     void Update()
     {
-        
+        distancePercentage += speed * Time.deltaTime / splineLength;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 currentPosition = spline.EvaluatePosition(distancePercentage);
+        Vector3 nextPosition = spline.EvaluatePosition(distancePercentage + 0.05f);
+        Vector3 direction = nextPosition - currentPosition;
+
+        transform.position = currentPosition;
+        transform.rotation = Quaternion.LookRotation(direction, transform.up);
     }
 }
