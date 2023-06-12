d642eaf8226585283ffdfd9a9e5963905c0387b5
Floha <fgulhan07@gmail.com>
2023-06-12 19:47:20 +0300

    hıı kadığğğn

---
+++ Assets/Scripts/SplineTracker.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplineTracker : MonoBehaviour
+{
+    public Spline[] spline;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
